8f10edc861ea1321f5c974bec81841eeda16ecbe
Lee Leahy <lpleahyjr@gmail.com>
2024-10-06 13:24:23 -1000

    SPI: Add support for R4A_SPI class

--- src/R4A_ESP32.h
+++ src/R4A_ESP32.h
@@ -504,6 +504,63 @@ void r4aEsp32NvmMenuWriteParameterFile(const struct _R4A_MENU_ENTRY * menuEntry,
                                        Print * display);
 
 //****************************************
+// SPI API
+//****************************************
+
+class R4A_ESP32_SPI : public R4A_SPI
+{
+  private:
+
+    uint32_t _div;
+    spi_t * _spi;
+
+  public:
+
+    // Allocate DMA buffer
+    // Inputs:
+    //   length: Number of data bytes to allocate
+    // Outputs:
+    //   Returns the buffer address if successful and nullptr otherwise
+    uint8_t * allocateDmaBuffer(int length)
+    {
+        return (uint8_t *)heap_caps_malloc(length, MALLOC_CAP_DMA);
+    }
+
+    // Initialize the SPI controller
+    // Inputs:
+    //   spiNumber: Number of the SPI controller
+    //   pinMOSI: SPI TX data pin number
+    //   clockHz: SPI clock frequency in Hertz
+    // Outputs:
+    //   Return true if successful and false upon failure
+    bool begin(uint8_t spiNumber, uint8_t pinMOSI, uint32_t clockHz)
+    {
+        // Determine the SPI clock divider
+        _div = spiFrequencyToClockDiv(clockHz);
+
+        // Configure the SPI device
+        _spi = spiStartBus(spiNumber, _div, SPI_MODE0, SPI_MSBFIRST);
+        if (_spi)
+        {
+            // Connect the SPI TX output to the MOSI pin
+            spiAttachMOSI(_spi, pinMOSI);
+            return true;
+        }
+        return false;
+    }
+
+    // Transfer the data to the SPI device
+    // Inputs:
+    //   txBuffer: Address of the buffer containing the data to send
+    //   rxBuffer: Address of the receive data buffer
+    //   length: Number of data bytes in the buffer
+    void transfer(const uint8_t * txBuffer, uint8_t * rxBuffer, uint32_t length)
+    {
+        spiTransferBytes(_spi, txBuffer, rxBuffer, length);
+    }
+};
+
+//****************************************
 // Timer API
 //****************************************
 
