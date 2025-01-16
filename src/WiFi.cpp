736b7fac6d4ebae56c40c4452d7deca58fb95980
Lee Leahy <lpleahyjr@gmail.com>
2025-01-15 16:37:41 -1000

    WiFi: Add WiFi authorization modes from ESP Arduino 3.1.1

--- src/WiFi.cpp
+++ src/WiFi.cpp
@@ -20,13 +20,15 @@ static const char * r4aEsp32WiFiAuthorizationName[] =
     "WPA_PSK",
     "WPA2_PSK",
     "WPA_WPA2_PSK",
-    "WIFI_AUTH_WPA_WPA2_PSK",
-    "WPA2_Enterprise",
+    "Enterprise",
     "WPA3_PSK",
     "WPA2_WPA3_PSK",
     "WAPI_PSK",
     "OWE",
     "WPA3_ENT_192",
+    "WPA3_EXT_PSK",
+    "WPA3_EXT_PSK_MIXED_MODE",
+    "DPP",
 };
 static const int r4aEsp32WiFiAuthorizationNameEntries =
     sizeof(r4aEsp32WiFiAuthorizationName) / sizeof(r4aEsp32WiFiAuthorizationName[0]);
