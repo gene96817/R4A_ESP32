ae2554502c9ed1f2885a34832bd6de2c0067e21f
gene96817 <71364333+gene96817@users.noreply.github.com>
2025-05-15 15:25:51 -1000

    Gene's .ino files now has a numerical prefix to help order functions to the compiler

--- examples/Freenove_4WD_Car/2SetMotor.ino
+++ examples/Freenove_4WD_Car/2SetMotor.ino
@@ -27,6 +27,24 @@ constexpr int MOTOR_4_DIRECTION = 1;
 
 //  bool trace = true;    // this enables all the print statements for debugging.
 
+// *************************************
+// logging startes here
+// *************************************
+
+bool datalogger(bool trace, int speed, float direction, int leftWheelSpeed, int rightWheelSpeed) {
+    if (trace) {
+        Serial.print("  Move ");
+        Serial.print(speed);
+        Serial.print("  ");
+        Serial.print(direction);
+        Serial.print("  Left Wheel ");
+        Serial.print(leftWheelSpeed);
+        Serial.print("  Right Wheel ");
+        Serial.print(rightWheelSpeed);
+    }
+    return true;
+}
+
 int speed;      //relative speed from -20 to 20
 int direction;  //degrees
 int DEGREE_TO_RADIAN = 2 * 3.1415 / 360;
@@ -57,6 +75,72 @@ bool Motor_Move(int left1, int left2, int right1, int right2) {
               << "Left2=" << left2 << ", "
               << "Right1=" << right1 << ", "
               << "Right2=" << right2 << "\n";
+
+/**********************************************************************
+  Basic_Line_Following.ino
+  File        :  Line Following Challenge ECv1
+  Author      :  Eugene Chang
+  Modification:  2025/03/02
+  Version     :  1
+
+  Perform basic line following, modified code from 04.2_Track_Car example
+  Line following logic by Eugene Chang
+
+  This code is for the Freenove 4WD Car with ESP32.
+  The robot is expected to follow a black line on a white background.
+  The robot will stop if it loses the track.
+  The robot will search for the track if it loses it.
+
+**********************************************************************/
+
+//****************************************
+// Constants
+//****************************************
+
+//  #include "SetMotor.ino"
+// #include "seeTrack.ino"
+
+#define glf_DEBUG_MOTORS 0
+// #define glf_DEBUG_STATES 0
+
+#define glf_SPEED_LV4 4000
+#define glf_SPEED_LV3 3000
+#define glf_SPEED_LV2 2500
+#define glf_SPEED_LV1 1500
+#define glf_SPEED_MIN 1500  // This can be varied according to the motor performance \
+                            // Set this large enough to get consistent readings motor movement
+
+#define BLT_SPEED_FAST 1500
+#define BLT_SPEED_MEDIUM 1200
+#define BLT_SPEED_SLOW 1000
+
+#define BLT_TURN_THRESHOLD 100
+
+const int NUM_SENSORS = 6;
+
+
+//****************************************
+// Global; Variables
+//****************************************
+
+bool trace = true;  // this enables all the print statements for debugging.
+
+// glfTimeBefore is defined in Menu.ino
+// int glfTimeBefore = 0;  //Record each non-blocking time
+// int glfTimeCount = 0;   //Record the number of non-blocking times
+// int glfTimeFlag = 0;    //Record the blink time
+
+//initialize memory of track detection
+int sensorValue[3];
+int SensorReadings[6] = { 2, 2, 2, 2, 0, 0 };  // last 6 positions read
+int theta[2] = { 0, 0 };                       // last 2 theta (headings) readings
+
+// int speed;        //relative speed from -20 to 20
+// float direction;  //degrees
+// int leftWheelSpeed;
+// int rightWheelSpeed;
+
+
 
 
 
